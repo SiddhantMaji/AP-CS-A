e316e1615f083e8efa6cf80159aba58ddaa69ad9
SiddhantMaji <siddhant.maji@gmail.com>
2022-12-11 21:38:59 -0600

    primitive data types

--- Unit1/Two.java
+++ Unit1/Two.java
@@ -1,7 +1,19 @@
+//Unit 1.2 - Variables and Data Types
+//Date - December 11, 2022
+
 package Unit1;
 
 public class Two {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Primitive Data Types - String, Integer, and Boolean
+        String s1;
+        int i1;
+        boolean b1;
+
+        s1 = "wow";
+        i1 = 2;
+        b1 = (i1 == 2);
+
+        System.out.println(s1 + "\n" + i1 + "\n" + b1);
     }
 }

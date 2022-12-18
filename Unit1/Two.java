170b32c062f082aef596691a20f395d3a2319802
SiddhantMaji <siddhant.maji@gmail.com>
2022-12-17 22:12:38 -0600

    finished unit 1.2 and added unit 1 description

--- Unit1/Two.java
+++ Unit1/Two.java
@@ -5,15 +5,30 @@ package Unit1;
 
 public class Two {
     public static void main(String[] args) {
-        //Primitive Data Types - String, Integer, and Boolean
-        String s1;
+        //Primitive Data Types - Double, Integer, and Boolean
+        double d1;
         int i1;
         boolean b1;
 
-        s1 = "wow";
+        d1 = 2.5;
         i1 = 2;
         b1 = (i1 == 2);
 
-        System.out.println(s1 + "\n" + i1 + "\n" + b1);
+        System.out.println(d1 + "\n" + i1 + "\n" + b1);
+
+        //A type is a set of values and a set of operations on them
+        String s2 = "wow";
+        System.out.println(s2); //Prints value of s2
+        System.out.println(s2.charAt(1)); //charAt (prints character at specified index)
+
+        //Data types can be categorized as either primitive or reference
+        //Each variable has associated memory
+        //Memory of primitive type variable holds actual primitive value
+
+        //Final keyword creates an immutable constant variable
+        final int i2 = 4;
+        //i2 = 5;
+        //^^^ Returns an error: The final local variable i2 cannot be assigned. It must be blank and not using a compound assignment
+        
     }
 }
